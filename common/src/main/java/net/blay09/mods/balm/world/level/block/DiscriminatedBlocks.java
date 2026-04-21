248b593afa7907d97575d1176d1b7f97fbc32eff
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-22 00:22:55 +0200

    refactor(26.2)!: Remove deprecated prefix/suffix methods in DiscriminatedBlocks and DiscriminatedItems

--- common/src/main/java/net/blay09/mods/balm/world/level/block/DiscriminatedBlocks.java
+++ common/src/main/java/net/blay09/mods/balm/world/level/block/DiscriminatedBlocks.java
@@ -1,11 +1,7 @@
 package net.blay09.mods.balm.world.level.block;
 
-import net.minecraft.util.StringUtil;
-import org.jspecify.annotations.Nullable;
-
 import java.util.Comparator;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -23,36 +19,6 @@ public interface DiscriminatedBlocks<T> extends Map<T, DeferredBlock> {
 
     default Stream<DeferredBlock> sortedValues() {
         return sortedEntries().map(Entry::getValue);
-    }
-
-    @Deprecated
-    static <T> String prefix(@Nullable T value, String name) {
-        return value == null || StringUtil.isBlank(Objects.toString(value)) ? name : value + "_" + name;
-    }
-
-    @Deprecated
-    static <T> String suffix(String name, @Nullable T value) {
-        return value == null || StringUtil.isBlank(Objects.toString(value)) ? name : name + "_" + value;
-    }
-
-    @Deprecated
-    static <T> Function<T, String> prefixer(String name) {
-        return prefixer(name, null);
-    }
-
-    @Deprecated
-    static <T> Function<T, String> suffixer(String name) {
-        return suffixer(name, null);
-    }
-
-    @Deprecated
-    static <T> Function<T, String> prefixer(String name, @Nullable T skip) {
-        return it -> prefix(it != skip ? it : null, name);
-    }
-
-    @Deprecated
-    static <T> Function<T, String> suffixer(String name, @Nullable T skip) {
-        return it -> suffix(name, it != skip ? it : null);
     }
 
     static <T> Function<T, String> prefixWith(String name) {

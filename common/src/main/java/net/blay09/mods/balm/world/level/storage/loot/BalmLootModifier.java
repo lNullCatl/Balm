b094d424ff5572e976e26ec2e9af38d72200c94c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-22 00:32:51 +0200

    refactor(26.2)!: Remove deprecated BalmLootModifier overload, now always takes a nullable lootTableId as third parameter

--- common/src/main/java/net/blay09/mods/balm/world/level/storage/loot/BalmLootModifier.java
+++ common/src/main/java/net/blay09/mods/balm/world/level/storage/loot/BalmLootModifier.java
@@ -7,18 +7,7 @@ import net.minecraft.world.level.storage.loot.LootTable;
 import org.jspecify.annotations.Nullable;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BalmLootModifier {
-    /**
-     * @deprecated Use {@link #apply(LootContext, List, ResourceKey)} which receives a loot table id, if available.
-     */
-    @Deprecated
-    default void apply(LootContext context, List<ItemStack> loot) {
-        apply(context, loot, null);
-    }
-
-    default void apply(LootContext context, List<ItemStack> loot, @Nullable ResourceKey<LootTable> lootTableId) {
-        apply(context, loot);
-    }
+    void apply(LootContext context, List<ItemStack> loot, @Nullable ResourceKey<LootTable> lootTableId);
 }

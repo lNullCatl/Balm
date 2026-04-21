060c03537a8dc55e129d6ba83850ea3a364dc923
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-22 00:17:03 +0200

    refactor(26.2): LevelRenderer -> LevelExtractor

--- common/src/main/java/net/blay09/mods/balm/internal/mixin/LevelExtractorMixin.java
+++ common/src/main/java/net/blay09/mods/balm/internal/mixin/LevelExtractorMixin.java
@@ -3,7 +3,7 @@ package net.blay09.mods.balm.internal.mixin;
 import net.blay09.mods.balm.client.platform.event.internal.BalmSupplementalClientEvents;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.extract.LevelExtractor;
 import net.minecraft.client.renderer.state.level.LevelRenderState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Final;
@@ -13,14 +13,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LevelRenderer.class)
-public class LevelRendererMixin {
+@Mixin(LevelExtractor.class)
+public class LevelExtractorMixin {
 
     @Final
     @Shadow
     private Minecraft minecraft;
 
-    @Inject(method = "extractBlockOutline(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/state/level/LevelRenderState;)V", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "extractBlockOutline(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/state/level/LevelRenderState;)V", at = @At("RETURN"))
     public void extractBlockOutline(Camera camera, LevelRenderState levelRenderState, CallbackInfo ci) {
         if (minecraft.hitResult instanceof BlockHitResult blockHitResult && levelRenderState.blockOutlineRenderState != null) {
             if (!BalmSupplementalClientEvents.RENDER_BLOCK_HIGHLIGHT.invoker()
--- fabric/src/main/java/net/blay09/mods/balm/fabric/internal/mixin/ClientLevelMixin.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/internal/mixin/ClientLevelMixin.java
@@ -3,7 +3,7 @@ package net.blay09.mods.balm.fabric.internal.mixin;
 import net.blay09.mods.balm.fabric.client.internal.event.FabricBalmSupplementalClientEvents;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.extract.LevelExtractor;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientLevel.class)
 public class ClientLevelMixin {
     @Inject(method = "<init>", at = @At("TAIL"))
-    private void onConstructor(ClientPacketListener clientPacketListener, ClientLevel.ClientLevelData clientLevelData, ResourceKey<Level> resourceKey, Holder<DimensionType> holder, int i, int j, LevelRenderer levelRenderer, boolean bl, long l, int i2, CallbackInfo ci) {
+    private void onConstructor(ClientPacketListener connection, ClientLevel.ClientLevelData levelData, ResourceKey<Level> dimension, Holder<DimensionType> dimensionType, int serverChunkRadius, int serverSimulationDistance, LevelExtractor levelExtractor, boolean isDebug, long biomeZoomSeed, int seaLevel, CallbackInfo ci) {
         ClientLevel clientLevel = (ClientLevel) (Object) this;
         FabricBalmSupplementalClientEvents.CLIENT_LEVEL_LOAD.invoker().handle(clientLevel);
     }
